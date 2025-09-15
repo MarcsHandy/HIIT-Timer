287c5bba0e4deb4669cb475e83f908ff768a28a7
Heru <rumblereflex@gmail.com>
2025-09-15 20:58:48 +0300

    Initial Commit

---
+++ HIIT Timer/HIIT_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIIT_TimerApp.swift
+//  HIIT Timer
+//
+//  Created by Heru on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HIIT_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HIIT TimerTests/HIIT_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HIIT_TimerTests.swift
+//  HIIT TimerTests
+//
+//  Created by Heru on 9/15/25.
+//
+
+import Testing
+@testable import HIIT_Timer
+
+struct HIIT_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
